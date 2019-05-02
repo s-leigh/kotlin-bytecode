b07bb8587b2b93bfcef027b82b1781a87a0f181c
Stephen Leigh <s-leigh@users.noreply.github.com>
2019-05-02 15:04:57 +0100

    Extend tailrec

--- src/tailrec/tailrec.kt
+++ src/tailrec/tailrec.kt
@@ -1,15 +1,16 @@
-fun sumOfInts(limit: Int, runningTotal: Int): Int {
-    return if (limit == 1) {
-        runningTotal + limit
-    } else {
-        sumOfInts(limit - 1, limit + runningTotal)
-    }
+package src.tailrec
+
+fun sumOfInts(limit: Int): Int {
+    if (limit == 1) return limit
+    return limit + sumOfInts(limit - 1)
 }
 
 tailrec fun sumOfIntsTailRec(limit: Int, runningTotal: Int): Int {
-    return if (limit == 1) {
-        runningTotal + limit
-    } else {
-        sumOfIntsTailRec(limit - 1, limit + runningTotal)
-    }
+    if (limit == 1) return runningTotal + limit
+    return sumOfIntsTailRec(limit - 1, limit + runningTotal)
+}
+
+tailrec fun sumOfIntsTailRecWithDefaultParam(limit: Int, runningTotal: Int = 0): Int {
+    if (limit == 1) return runningTotal + limit
+    return sumOfIntsTailRecWithDefaultParam(limit - 1, limit + runningTotal)
 }
